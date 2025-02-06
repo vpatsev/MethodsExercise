4245d13d67658f0006854884d5bb77c17244097d
Ventsislav Patsev <164578745+vpatsev@users.noreply.github.com>
2025-02-06 03:21:25 +0200

    Add files via upload

---
+++ 04.PasswordValidator/Program.cs
@@ -0,0 +1,73 @@
+﻿
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string password = Console.ReadLine();
+        string result = ValdiatePassword(password);
+        Console.WriteLine(result);
+    }
+
+    static string ValdiatePassword(string password)
+    {
+        string result = "";
+        if (!IsValidLength(password))
+        {
+            result += $"Password must be between 6 and 10 characters" + Environment.NewLine;
+        }
+
+        if (!IsConsistsOfLettersAndDigits(password))
+        {
+            result += "Password must consist only of letters and digits" + Environment.NewLine;
+        }
+
+        if (!IsValidDigitsCount(password))
+        {
+            result += "Password must have at least 2 digits" + Environment.NewLine;
+        }
+
+        if (result == "")
+        {
+            result = "Password is valid";
+        }
+
+        return result;
+    }
+
+    private static bool IsConsistsOfLettersAndDigits(string password)
+    {
+        foreach (char letter in password)
+        {
+            if (!char.IsLetterOrDigit(letter))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static bool IsValidDigitsCount(string password)
+    {
+        int digits = 0;
+        foreach (char letter in password)
+        {
+            if (char.IsDigit(letter))
+            {
+                digits++;
+            }
+
+            if (digits == 2)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static bool IsValidLength(string password)
+    {
+        return password.Length >= 6 && password.Length <= 10;
+    }
+}
---
+++ 05.MultiplyEvensbyOdds/Program.cs
@@ -0,0 +1,32 @@
+﻿int num = int.Parse(Console.ReadLine());
+int result = GetMultipleOfEvenAndOdds(Math.Abs(num));
+Console.WriteLine(result);
+
+static int GetMultipleOfEvenAndOdds(int num)
+{
+    int evenDigits = GetSumOfEvenDigits(num);
+    int oddDigits = GetSumOfOddDigits(num);
+    return evenDigits * oddDigits;  
+}
+
+static int GetSumOfEvenDigits(int num   )
+{    int sumOfDigits = 0;
+    while(num > 0)
+    {
+        int lastDigit = num % 10;
+        if (num % 2 == 1) { sumOfDigits += lastDigit; }
+        num /= 10;  
+    }
+    return sumOfDigits;
+}  
+
+static int GetSumOfOddDigits(int num)
+{
+    int sumOfDigits = 0;
+    while (num > 0)
+    {    int lastDigit = num % 10;
+        if (num % 2 == 0) { sumOfDigits += lastDigit; }
+         num /= 10;
+    }
+    return sumOfDigits;
+}
